630bcb54c61f7a5f22f3be1330bb148f79fd2c5c
Michelle Huerta <michellehtrad@email.com>
2025-08-16 14:00:44 -0400

    Validación de entrada

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+const input = document.getElementById("amigo");
+const nombre = input.value.trim();
+
+//validación
+if (nombre === "") {
+    alert ("Por favor, inserte un nombre");
+    return; //detener funcion si está vacia
